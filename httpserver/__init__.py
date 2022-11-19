89af89c997f2c969b9a4bf62121d0f266955b969
Erik de Lange <erikdelange@users.noreply.github.com>
2022-11-19 21:58:55 +0100

    Delete __init__.py

--- httpserver/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2021 (c) Erik de Lange
-# Released under MIT license
-
-from .response import CRLF, MimeType, ResponseHeader, StatusLine
-from .sendfile import sendfile
-from .server import CONNECTION_CLOSE, CONNECTION_KEEP_ALIVE, Server
